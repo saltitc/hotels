aa424efed2898414ec39d44f89527c03a1ca4b8c
Ayvaz Izmailov <114296895+saltitc@users.noreply.github.com>
2023-11-30 20:13:42 +0300

    Exceptions are separated to exceptions.py. Added logout

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.bookings.dao import BookingDAO
 from app.bookings.schemas import SBooking
+from app.users.dependencies import get_current_user
+from app.users.models import Users
 
 router = APIRouter(prefix="/bookings", tags=["Бронирования"])
 
 
 @router.get("")
-async def get_bookings() -> list[dict[str, SBooking]]:
-    return await BookingDAO.find_all()
+async def get_bookings(user: Users = Depends(get_current_user)) -> list[dict[str, SBooking]]:
+    return await BookingDAO.find_all(user_id=user.id)
